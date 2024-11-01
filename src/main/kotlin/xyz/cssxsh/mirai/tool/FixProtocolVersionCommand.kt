15c991179e18811eb53de8b48947f18703baeb38
人間工作 <coolxiaom95@gmail.com>
2024-11-01 16:12:45 +0800

    add more commands with Chinese desc

--- src/main/kotlin/xyz/cssxsh/mirai/tool/FixProtocolVersionCommand.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/tool/FixProtocolVersionCommand.kt
@@ -1,11 +1,14 @@
 package xyz.cssxsh.mirai.tool
 
-import net.mamoe.mirai.console.command.*
-import net.mamoe.mirai.internal.spi.EncryptService
-import net.mamoe.mirai.utils.*
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.utils.BotConfiguration
+import xyz.cssxsh.mirai.tool.adapters.Client
 import java.io.File
 
 @PublishedApi
+@OptIn(ConsoleExperimentalApi::class)
 internal object FixProtocolVersionCommand : CompositeCommand(
     owner = FixProtocolVersionPlugin,
     "trpgBotSign",
@@ -13,7 +16,8 @@ internal object FixProtocolVersionCommand : CompositeCommand(
     description = "Plugin Command"
 ) {
     @SubCommand("fetch", "sync")
-    suspend fun CommandSender.fetch(protocol: BotConfiguration.MiraiProtocol, version: String = "latest") {
+    @Description("从远程仓库下载协议版本信息配置文件")
+    suspend fun CommandSender.fetch(@Name("协议类型") protocol: BotConfiguration.MiraiProtocol, @Name("版本号") version: String = "latest") {
         try {
             FixProtocolVersion.fetch(protocol, version)
             sendMessage(FixProtocolVersion.info()[protocol] ?: "找不到协议信息")
@@ -24,7 +28,25 @@ internal object FixProtocolVersionCommand : CompositeCommand(
     }
 
     @SubCommand
-    suspend fun CommandSender.load(protocol: BotConfiguration.MiraiProtocol, version: String? = null) {
+    @Description("查看可使用 load 命令加载的协议版本列表")
+    suspend fun CommandSender.list(@Name("协议类型") protocol: BotConfiguration.MiraiProtocol) {
+        val factory = NetworkServiceFactory.inst
+        if (factory == null) {
+            sendMessage("当前已有其它签名服务优先级高于 trpgbot 签名服务")
+            return
+        }
+        val files = factory.protocolsFolder.listFiles { _, name -> BotConfiguration.MiraiProtocol.values().any { name.startsWith(it.name + "_", true) } && name.endsWith(".json") }
+        sendMessage(buildString {
+            appendLine("协议版本列表如下:")
+            if (files != null) for (file in files) {
+                appendLine("- " + file.nameWithoutExtension)
+            }
+        })
+    }
+
+    @SubCommand
+    @Description("从本地加载协议版本信息配置文件，不填写版本使用 \"./协议类型.json\"；填写版本使用 \"./protocols/协议类型_协议版本.json\"")
+    suspend fun CommandSender.load(@Name("协议类型") protocol: BotConfiguration.MiraiProtocol, @Name("版本号") version: String? = null) {
         try {
             if (version != null) {
                 val factory = NetworkServiceFactory.inst
@@ -49,6 +71,7 @@ internal object FixProtocolVersionCommand : CompositeCommand(
     }
 
     @SubCommand
+    @Description("查看各协议版本信息")
     suspend fun CommandSender.info() {
         sendMessage(buildString {
             appendLine("当前各协议版本日期: ")
@@ -57,7 +80,36 @@ internal object FixProtocolVersionCommand : CompositeCommand(
             }
         })
     }
+
+    @SubCommand("websocket", "ws")
+    @Description("查看ws连接列表")
+    suspend fun CommandSender.websocket() {
+        sendMessage(buildString {
+            appendLine("当前 WebSocket 连接池情况：")
+            for ((address, client) in Client.connectionPool) {
+                val status = if (client.isOpen) "已连接" else "未连接"
+                appendLine("- $status $address")
+            }
+        })
+    }
+
     @SubCommand
+    @Description("断开ws连接。你不应该在该链接被某Bot使用时执行该操作")
+    suspend fun CommandSender.disconnect(@Name("地址") address: String) {
+        val client = Client.connectionPool[address]
+        if (client == null) {
+            sendMessage("未在 WebSocket 连接池中找到该连接")
+        } else if (!client.isOpen) {
+            sendMessage("该连接早已断开")
+        } else {
+            NetworkServiceFactory.created
+            client.closeBlocking(1000, "用户请求关闭")
+            sendMessage("已成功断开连接")
+        }
+    }
+
+    @SubCommand
+    @Description("重载配置文件 network.json")
     suspend fun CommandSender.reload() {
         val factory = NetworkServiceFactory.inst
         if (factory == null) {
